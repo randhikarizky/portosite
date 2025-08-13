d9cb7d04d68c0ae4e3b208fc7b0a25ab6c01bc38
Randhika Rizkyaldi <rizkyran.rr@gmail.com>
2025-08-13 13:49:55 +0700

    Create index.tsx

---
+++ pages/portofolio/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Portofolio() {
+  return (
+    <div style={{ height: '100vh', width: '100%', overflow: 'hidden' }}>
+      <iframe
+        src="https://randhikar.notion.site/Hello-1e014143f2658092b5e0e7f54c83fe99"
+        style={{
+          border: 'none',
+          width: '100%',
+          height: '100%',
+        }}
+        sandbox="allow-scripts allow-same-origin allow-popups allow-forms"
+      />
+    </div>
+  );
+}
